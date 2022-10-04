fe30385d0f73d87ec34dfe2e5a0ff51c5fbf9881
yuanshuai <yuanshuai@gyyx.cn>
2022-10-04 22:13:40 +0800

    读取本地文件内容

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main () {
     // 命令行接受输入
@@ -9,4 +10,9 @@ fn main () {
 
     println!("Search for {}", query);
     println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename)
+    .expect("Something went wrong reading the file");
+
+    println!("With text \n{}", contents);
 }
