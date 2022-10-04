52887e18d93c3cfaddb6b476c97a1a03b0cb60dc
yuanshuai <yuanshuai@gyyx.cn>
2022-10-04 22:31:39 +0800

    函数重构

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,15 +4,31 @@ use std::fs;
 fn main () {
     // 命令行接受输入
     let args: Vec<String> = env::args().collect();
-    // 获取输入参数的第一个和第二个
-    let query = &args[1];
-    let filename = &args[2];
 
-    println!("Search for {}", query);
-    println!("In file {}", filename);
-
-    let contents = fs::read_to_string(filename)
+    let config = Config::new(&args);
+    let contents = fs::read_to_string(config.filename)
     .expect("Something went wrong reading the file");
 
     println!("With text \n{}", contents);
 }
+
+// 构造函数
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    // 转换配置函数
+    fn new(args: &[String]) -> Config {
+        // 获取输入参数的第一个和第二个
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        // 返回结构体
+        Config { 
+            query: query, 
+            filename: filename 
+        }
+    }
+}
+
